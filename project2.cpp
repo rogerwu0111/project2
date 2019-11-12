5fd41cc90030c79f4ae217de5d733587811643e9
RongJheWu <rogerwu0111@gmail.com>
2019-11-12 23:16:40 +0800

    distance and predecessor class

--- project2.cpp
+++ project2.cpp
@@ -2,9 +2,82 @@
 
 using namespace std;
 
-int main(void)
+
+// used to store the information of distance and predecessor
+class block
 {
-    cout << "Hello world!" << endl;
+private:
+    int Distance;
+    block* Predecessor;
+public:
+
+    block(int dis=-1, block* pre=NULL):Distance(dis), Predecessor(pre){}
+
+    void setDistance(int dis){this->Distance = dis;}
+
+    void setPredecessor(block* pre){this->Predecessor = pre;}
+
+    int getDistance(){return this->Distance;}
+
+    block* getPredecessor(){return this->Predecessor;}
+};
+
+void Initialize_BlockArray(int row, int col, block**arr, char** input)
+{
+    for (int i=0; i<row; i++)
+    {
+        for (int j=0; j<col; j++)
+        {
+            if (input[i][j] == '0') arr[i][j].setDistance(0);
+
+            else if (input[i][j] == '1');
+
+            else
+            {
+                arr[i][j].setDistance(0);
+
+                arr[i][j].setPredecessor(&arr[i][j]);
+            }
+        }
+    }
+}
+
+void BlockArray(int row, int col, block**arr, char** input)
+{
+
+}
+
+int main()
+{
+    int rows, columns;
+
+    cin >> rows;
+
+    cin >> columns;
+
+    char** input = new char*[rows];
+
+    for (int i=0; i<rows; i++)
+    {
+        input[i] = new char[columns];
+    }
+
+    for (int i=0; i<rows; i++)
+    {
+        for (int j=0; j<columns; j++)
+        {
+            cin >> input[i][j];
+        }
+    }
+
+    block** dis_pre = new block* [rows];
+
+    for (int i=0; i<rows; i++)
+    {
+        dis_pre[i] = new block[columns];
+    }
+
+    Initialize_BlockArray(rows, columns, dis_pre, input);
 
     return 0;
 }
